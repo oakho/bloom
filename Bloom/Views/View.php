7ff576d1625f16ce43e1fa348ba660d7e8b80050
Oakho <hello@oakho.com>
2012-07-08 11:56:07 +0200

    Views classes normalization tab width 2 spaces

--- Bloom/Views/View.php
+++ Bloom/Views/View.php
@@ -4,27 +4,27 @@ namespace Bloom\Views;
 
 class View
 {
-    protected $_path = "";
+  protected $_path = "";
 
-    public function __contruct() {  }
+  public function __contruct() {  }
 
-    public function setPath($path)
-    {
-        $this->_path = $path;
+  public function setPath($path)
+  {
+    $this->_path = $path;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getPath()
-    {
-        return $this->_path;
-    }
+  public function getPath()
+  {
+    return $this->_path;
+  }
 
-    public function render()
-    {
-        ob_start();
-        require VIEWS_PATH . DS . $this->_path .".phtml";
+  public function render()
+  {
+    ob_start();
+    require VIEWS_PATH . DS . $this->_path .".phtml";
 
-        return ob_get_clean();
-    }
+    return ob_get_clean();
+  }
 }
